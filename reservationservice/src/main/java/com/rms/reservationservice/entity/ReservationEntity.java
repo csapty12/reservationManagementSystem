b5003ad8e955108721499748b51cde28195f5dce
Carl Saptarshi <csapty12@gmail.com>
2021-01-10 20:49:07 +0000

    PART-1: created the reservationEntity object

--- reservationservice/src/main/java/com/rms/reservationservice/entity/ReservationEntity.java
+++ reservationservice/src/main/java/com/rms/reservationservice/entity/ReservationEntity.java
@@ -1,4 +1,25 @@
 package com.rms.reservationservice.entity;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name = "reservation")
 public class ReservationEntity {
+
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private LocalDateTime reservationTime;
+    private int numberOfGuests;
+
 }
