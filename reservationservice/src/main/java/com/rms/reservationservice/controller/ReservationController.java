019c05544d1603f0a0ed33204e80dd6faa601fa8
Carl Saptarshi <csapty12@gmail.com>
2021-01-14 18:50:04 +0000

    PART-5: implementing PUT operations to update a reservation

--- reservationservice/src/main/java/com/rms/reservationservice/controller/ReservationController.java
+++ reservationservice/src/main/java/com/rms/reservationservice/controller/ReservationController.java
@@ -25,4 +25,10 @@ public class ReservationController {
     public Reservation get(@PathVariable("reservationId") String reservationId) {
         return reservationService.getReservationById(reservationId);
     }
+
+    @PutMapping("/{reservationId}")
+    @ResponseStatus(HttpStatus.OK)
+    public Reservation update(@PathVariable("reservationId") String reservationId, @RequestBody Reservation reservation) {
+        return reservationService.updateReservation(reservationId, reservation);
+    }
 }
